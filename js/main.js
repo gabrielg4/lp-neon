987beb96ed5e3761473da75b8282a8ff8eaa0e51
Gabriel Guimel <gabriel.guimelrn@gmail.com>
2021-10-26 21:37:39 -0300

    adicionando o menu fixed no header

--- js/main.js
+++ js/main.js
@@ -8,3 +8,15 @@ function openDropdown(e) {
 
 btnDropdown.addEventListener('click', openDropdown);
 dropdown.addEventListener('mouseleave', openDropdown);
+
+const header = document.getElementById('js-header');
+
+function fixedMenu() {
+  if(window.pageYOffset > 80) {
+    header.classList.add('fixed-menu');
+  } else {
+    header.classList.remove('fixed-menu');
+  }
+}
+
+document.addEventListener('scroll', fixedMenu);
