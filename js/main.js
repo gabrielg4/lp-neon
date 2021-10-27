13160fcc591d5a696d11bed34c7c7121ecba1851
Gabriel Guimel <gabriel.guimelrn@gmail.com>
2021-10-26 22:20:36 -0300

    update no menu mobile

--- js/main.js
+++ js/main.js
@@ -20,3 +20,23 @@ function fixedMenu() {
 }
 
 document.addEventListener('scroll', fixedMenu);
+
+const btnMenu = document.getElementById('js-btn-menu-mobile');
+const overlayMenu = document.querySelector('.js-overlay');
+
+function openMenuMobile() {
+  document.documentElement.classList.add('menu-opened');
+}
+
+function closeMenuMobile() {
+  document.documentElement.classList.remove('menu-opened');
+}
+
+btnMenu.addEventListener('click', openMenuMobile);
+overlayMenu.addEventListener('click', closeMenuMobile);
+
+const accordion = document.querySelector('.js-accordion');
+
+function openAccordion() {
+  
+}
